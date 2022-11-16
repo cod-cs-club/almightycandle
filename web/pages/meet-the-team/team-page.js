81ccba25a1854eb01be71834f835f2480b3d8883
ashafiuddin21 <102944317+ashafiuddin21@users.noreply.github.com>
2022-11-16 10:39:29 -0600

    Just some structural code for the Meet The Team portion.

---
+++ web/pages/meet-the-team/team-page.js
@@ -0,0 +1,47 @@
+// This is the JS code for the Meet The Team Page on the Front-end AI website. 
+// Team leads: Dimitri, Sebastian
+// Team members(as I remember): Ayesha, Erikas, James. & Isabella, Ethan, Zaid.  
+
+// Sample code below
+
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="kitty.jpg" alt="Jane" style="width:100%"> </img>
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO &amp; Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="kitty.jpg" alt="Mike" style="width:100%"> </img>
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="kitty.jpg" alt="John" style="width:100%"> </img>
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
